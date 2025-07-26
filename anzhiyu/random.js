c5221aa368ed58cb3b5bb93b6d83406a208f3fcc
codecatzjy <1961675277@qq.com>
2025-07-26 19:22:59 +0800

    Site updated: 2025-07-26 19:22:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/26/hello-world/"];function toRandomPost(){
+var posts=["2025/07/26/hello-world/","2025/07/26/欢迎来到codecatzjy的博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
