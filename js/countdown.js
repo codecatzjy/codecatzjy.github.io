258d1e616e68b8ac5b36906358a206066c99d796
codecatzjy <1961675277@qq.com>
2025-07-30 11:34:46 +0800

    Site updated: 2025-07-30 11:34:39

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,7 @@
 const CountdownTimer = (() => {
     const config = {
         targetDate: "2026-02-17",
-        targetName: "һ",
+        targetName: һ,
         units: {
             day: { text: "����", unit: "Сʱ" },
             week: { text: "����", unit: "��" },
